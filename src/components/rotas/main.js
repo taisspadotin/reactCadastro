0bf9cdc97e9fa3d0e5fd0077b52c68d7a74a21df
taisspadotin <52021076+taisspadotin@users.noreply.github.com>
2020-03-10 17:02:53 -0300

    Add files via upload

--- src/components/rotas/main.js
+++ src/components/rotas/main.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from '../home/home'
 import Cadastro from '../cadastro/cadastro';
+import Buscar from '../buscar/buscar';
+
 const Main = () =>(
 	<Switch>
 	<Route exact path="/" component={Home} />
 	<Route path="/cadastro" component={Cadastro} />
+	<Route path="/buscar" component={Buscar} />
 	</Switch>
 )
 export default Main;
