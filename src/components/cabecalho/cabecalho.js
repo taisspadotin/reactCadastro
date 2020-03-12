d98cdbd632243622c8b26881d4fe37f2cd89b422
taisspadotin <52021076+taisspadotin@users.noreply.github.com>
2020-03-12 14:04:57 -0300

    Add files via upload

--- src/components/cabecalho/cabecalho.js
+++ src/components/cabecalho/cabecalho.js
@@ -4,16 +4,29 @@ import './style.scss';
 import {Link} from 'react-router-dom';
 
 class Cabecalho extends Component{
+		constructor(props){
+			super(props);
+			this.state = {
+				icone: false
+			};
+		}
 		render(){
+			let icone = '';
+			if(this.state.icone === false){
+				icone = <i className="folder outline icon" onClick={()=>this.setState({icone:true})}></i>;
+			}
+			else{
+				icone = <i className="folder open outline icon" onClick={()=>this.setState({icone:false})}></i>;
+			}
 			return(
 			<>
 				<Navbar className="cabecalho_Principal" expand="lg">
-				  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+				  <Navbar.Toggle>{icone}</Navbar.Toggle>
 				  <Navbar.Collapse id="basic-navbar-nav" >
 					<Nav className="mr-auto" style={{width:'100%', justifyContent: 'center', alignItems: 'center'}}>
 					 <Link to="/">Home</Link>
 					 <Link to="cadastro">Cadastro</Link>
-					  <Link to="/">Buscar</Link>
+					  <Link to="buscar">Buscar</Link>
 					  {/*<NavDropdown title="Buscar" id="basic-nav-dropdown">
 						<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
 						<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
